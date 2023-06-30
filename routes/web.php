3245dbd26014afd52b38fdcf9812f3ddac8b0e5b
Ubuntu <softgetix@gmail.com>
2023-06-30 11:48:07 +0530

    admin

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
+
+Route::group(['middleware' => ['is_admin']], function () {
+
+});
